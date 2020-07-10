15c4886ec5047530250eed0c0622b8ed651bb60f
tanishqv10 <tanishqvarshney.153@gmail.com>
2020-07-10 20:10:41 +0530

    create week 5

---
+++ Building Database Application in PHP/week 4/login.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+if (isset($_POST['cancel'])) {
+    header("Location: index.php");
+    return;
+}
+
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';  // Pw is php123
+
+$failure = false;  // If we have no POST data
+
+if (isset($_POST['email']) && isset($_POST['pass'])) {
+    if (strlen($_POST['email']) < 1 || strlen($_POST['pass']) < 1) {
+        $_SESSION['error'] = "User name and password are required";
+    } else if (strpos($_POST['email'], "@") === false) {
+        $_SESSION['error'] = "Email must have an at-sign (@)";
+        header("Location: login.php");
+        return;
+    } else {
+        $check = hash('md5', $salt . $_POST['pass']);
+        if ($check == $stored_hash) {
+            error_log("Login success ".$_POST['email']);
+            $_SESSION['name'] = $_POST['email'];
+            header("Location: view.php");
+            return;
+        } else {
+            $_SESSION['error'] = "Incorrect password";
+            error_log("Login fail ".$_POST['email']." $check");
+            header("Location: login.php");
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once "bootstrap.php"; ?>
+    <title>Tanishq Varshney (871035ec)'s Login Page</title>
+</head>
+<body>
+<div class="container">
+    <h1>Please Log In</h1>
+    <?php
+    if ( isset($_SESSION['error']) ) {
+        echo('<p style="color: red;">'.htmlentities($_SESSION['error'])."</p>\n");
+        unset($_SESSION['error']);
+    }
+    ?>
+    <form method="POST" action="login.php">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email"><br/>
+        <label for="id_1723">Password</label>
+        <input type="text" name="pass" id="id_1723"><br/>
+        <input type="submit" value="Log In">
+        <input type="submit" name="cancel" value="Cancel">
+    </form>
+    <p>
+        For a password hint, view source and find a password hint
+        in the HTML comments.
+        <!-- Hint: The password is the three character word of a language which you are using followed by 123. -->
+    </p>
+</div>
+</body>
